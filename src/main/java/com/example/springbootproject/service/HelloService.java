8bb4a3bbae1729d4c49895ab4068bcd059995ca2
Suraj K R <suraj.k@pharmeasy.in>
2025-01-01 13:15:07 +0530

    edited output text

--- src/main/java/com/example/springbootproject/service/HelloService.java
+++ src/main/java/com/example/springbootproject/service/HelloService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
     public String getGreeting() {
-        return "Hello, Spring Boottttt!";
+        return "Hello, Spring Boot!";
     }
 }
